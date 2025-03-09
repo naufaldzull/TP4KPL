3dc44207814a4dbd514d050deaf4c60c9ea1596e
naufa <naufal.mdzulfik@gmail.com>
2025-03-09 19:48:15 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4
+{
+    class KodePos
+    {
+        public enum kelurahan{
+            Batununggal,
+            Kujangsari,
+            Mengger,
+            Wates,
+            Cijaura,
+            Jatisari,
+            Margasari,
+            Sekejati,
+            Kebonwaru,
+            Maleer,
+            Samoja
+        }
+
+        public static string GetKodePos(kelurahan kelurahan)
+        {
+            string[] kodePos = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274", "40273" };
+
+            return kodePos[(int)kelurahan];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using TP4;
+
+Console.Write("Masukan nama kelurahan: ");
+string inputkelurahan = Console.ReadLine();
+
+if (Enum.TryParse(inputkelurahan, out KodePos.kelurahan daerah))
+{
+    string kodePos = KodePos.GetKodePos(daerah);
+    Console.WriteLine($"Kode Pos {inputkelurahan} : {kodePos}");
+} 
+else
+{
+    Console.WriteLine($"Kode Pos {inputkelurahan} : tidak ditemukan");
+}
